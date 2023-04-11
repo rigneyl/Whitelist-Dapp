5efa1266193c630bf30ccdc4e77902cea62eb017
rigneyl <33796668+rigneyl@users.noreply.github.com>
2023-04-11 17:12:25 +1000

    Update to Sepolia Chain
    Chained ref from Goerli to Seploa chain

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -35,11 +35,11 @@ export default function Home() {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
-    // If user is not connected to the Goerli network, let them know and throw an error
+    // If user is not connected to the Sepolia network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 5) {
-      window.alert("Change the network to Goerli");
-      throw new Error("Change network to Goerli");
+    if (chainId !== 11155111) {
+      window.alert("Change the network to Sepolia");
+      throw new Error("Change network to Sepolia");
     }
 
     if (needSigner) {
